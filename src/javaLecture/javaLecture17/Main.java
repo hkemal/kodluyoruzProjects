e5aa035586d4f4fbc59c996e757ce6cee09d221e
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-08-28 22:09:14 +0300

    017-kdv-tutarı-hesaplayan-program örnek kod yazıldı

---
+++ src/javaLecture/javaLecture17/Main.java
@@ -0,0 +1,22 @@
+package javaLecture.javaLecture17;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double kdvOrani = 0.18;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Tutar giriniz : ");
+        double tutar = input.nextInt();
+
+        double kdvTutar = tutar * kdvOrani;
+        double kdvliTutar = kdvTutar + tutar;
+
+        System.out.println("KDV'siz Tutar : " + tutar);
+        System.out.println("KDV Oranı : " + kdvOrani);
+        System.out.println("KDV Tutarı : " + kdvTutar);
+        System.out.println("KDV'li tutarı : " + kdvliTutar);
+    }
+}
