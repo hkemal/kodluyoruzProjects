029ec0cb96fc23647e15cc7ad2912cd4d7fb0ec5
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-09-05 11:04:24 +0300

    062-siniflar ögrenci bilgi sistemi

---
+++ src/java101Lecture/javaLecture62/Teacher.java
@@ -0,0 +1,16 @@
+package java101Lecture.javaLecture62;
+
+public class Teacher {
+
+    String name;
+
+    String phoneNumber;
+
+    String branch;
+
+    public Teacher(String name, String phoneNumber, String branch) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.branch = branch;
+    }
+}
