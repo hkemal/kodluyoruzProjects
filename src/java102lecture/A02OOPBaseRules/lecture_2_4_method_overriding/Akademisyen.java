a6b259298c8d4955f2015b985e251fdee000d6af
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-10-03 08:25:31 +0300

    Java-102-sample-codes-2-4-method-overriding

---
+++ src/java102lecture/A02OOPBaseRules/lecture_2_4_method_overriding/Akademisyen.java
@@ -0,0 +1,39 @@
+package java102lecture.A02OOPBaseRules.lecture_2_4_method_overriding;
+
+public class Akademisyen extends Calisan {
+
+    private String bolum;
+
+    private String unvan;
+
+    public Akademisyen(String adSoyad, String telefon, String eposta, String bolum, String unvan) {
+        super(adSoyad, telefon, eposta);
+        this.bolum = bolum;
+        this.unvan = unvan;
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public String getUnvan() {
+        return unvan;
+    }
+
+    public void setUnvan(String unvan) {
+        this.unvan = unvan;
+    }
+
+    public void derseGir() {
+        System.out.println(this.getAdSoyad() + " derse giriş yaptı.");
+    }
+
+    @Override
+    public void giris() {
+        System.out.println(this.getAdSoyad() + " akademisyeni A kapısından giriş yaptı.");
+    }
+}
---
+++ src/java102lecture/A02OOPBaseRules/lecture_2_4_method_overriding/GuvenlikGorevlisi.java
@@ -0,0 +1,23 @@
+package java102lecture.A02OOPBaseRules.lecture_2_4_method_overriding;
+
+public class GuvenlikGorevlisi extends Memur {
+
+    private String belge;
+
+    public GuvenlikGorevlisi(String adSoyad, String telefon, String eposta, String departman, String mesai, String belge) {
+        super(adSoyad, telefon, eposta, departman, mesai);
+        this.belge = belge;
+    }
+
+    public String getBelge() {
+        return belge;
+    }
+
+    public void setBelge(String belge) {
+        this.belge = belge;
+    }
+
+    public void nobet() {
+        System.out.println(this.getAdSoyad() + " nöbete başladı.");
+    }
+}
