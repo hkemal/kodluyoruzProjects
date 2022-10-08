a9bf7d0e5c276200f713198b992f64e44ec90bbb
hkemal <hasankemal96@gmail.com>
2022-10-09 00:07:53 +0300

    homework1.sql

--- src/sql/homework1.sql
+++ src/sql/homework1.sql
@@ -6,8 +6,7 @@ AND length <75;
 
 SELECT * FROM film
 WHERE rental_rate = 0.99
-AND (replacement_cost = 12.99
-OR replacement_cost = 28.99);
+AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 
 SELECT *  FROM customer
 WHERE first_name = 'Mary';
