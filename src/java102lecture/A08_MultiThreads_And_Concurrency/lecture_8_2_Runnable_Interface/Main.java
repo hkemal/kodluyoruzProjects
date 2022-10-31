e293635ac8dbac57aee256931959cd1c9cdecd70
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-10-31 17:46:48 +0300

    JAVA-102-sample-codes-lecture-8-1-and-8-2

---
+++ src/java102lecture/A08_MultiThreads_And_Concurrency/lecture_8_2_Runnable_Interface/Main.java
@@ -0,0 +1,16 @@
+package java102lecture.A08_MultiThreads_And_Concurrency.lecture_8_2_Runnable_Interface;
+
+public class Main {
+    public static void main(String[] args) {
+        Counter c1 = new Counter("counter-1");
+        Counter c2 = new Counter("counter-2");
+        Counter c3 = new Counter("counter-3");
+
+        Thread t1 = new Thread(c1);
+        Thread t2 = new Thread(c2);
+        Thread t3 = new Thread(c3);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
